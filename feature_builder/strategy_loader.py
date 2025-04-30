1d687f8e2df50a5740f6e870b62e3e11e935e6a0
Swapnil Singh <swapnilsingh2005@yahoo.co.in>
2025-05-01 02:05:13 +0530

    Removed unused code

--- feature_builder/strategy_loader.py
+++
@@ -1,6 +0,0 @@
-import yaml
-
-def load_strategy_config(path="configs/strategy.yaml"):
-    with open(path, "r") as f:
-        config = yaml.safe_load(f)
-    return config
--- tests/test_reward.py
+++
@@ -1,5 +0,0 @@
-from utils.reward import compute_reward
-
-def test_reward_computation():
-    assert round(compute_reward(100, 110), 2) == 0.1
-    assert round(compute_reward(100, 90), 2) == -0.1
